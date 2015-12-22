0f87d31ccf660ccd5c8bf7cb8ccbda716c41da45
anuragsiddula <anuragsiddula@gmail.com>
2015-12-21 19:07:53 -0500

    Design: First Commit-3 view controllers

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AtoZ
+//
+//  Created by Anurag Siddula on 2015-12-12.
+//  Copyright (c) 2015 Anurag Siddula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ screen2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  screen2ViewController.swift
+//  AtoZ
+//
+//  Created by Anurag Siddula on 2015-12-12.
+//  Copyright (c) 2015 Anurag Siddula. All rights reserved.
+//
+
+import UIKit
+
+class screen2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
